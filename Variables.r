ce7e6091243468e5a620cc5cc628e55862c6d482
Siddharth <33184198+iamSiddharth08@users.noreply.github.com>
2020-09-20 04:12:22 +0530

    First Assignment
    This is the Pirple Assignment no 1 solutions

---
+++ Variables.r
@@ -0,0 +1,6 @@
+a <- 7
+b <- "This is R"
+c <- c(15,2,78,96,36,12)
+e <- c("t", "es", "t", "a", "b", "c")
+f <-  data.frame(col1 = c, col2 = e)
+g <- list('a','b','c','d','e','f')
---
+++ song.R
@@ -0,0 +1,23 @@
+# My favorite song name its in Hindi by the way (STRING)
+ songName <- "Saathiyaa"
+
+# Year released (INTEGER)
+yearReleased <- 2002
+
+# Name of singer (STRING)
+artist <- "Sonu Nigam"
+
+# Genre (STRING)
+genre <- "Romantic"
+
+# Length (FLOAT)
+len <- "5.27"
+
+# Is available in English (BOOLEAN)
+isAvailableInEnglish <- F
+
+# More songs from the artist (LIST)
+moreSongsList <- list("Sapna Jaha","Sooraj hua matham","Dil ne ye kaha hai dil se")
+
+# Artist bio data (VECTOR)
+artistBioData <- c("Sonu Nigam", "47 Years old","Resides in Mumbai")
